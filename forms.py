895a5316943da3c40c73d6868599533c88ad6f2a
IDGS801-19002046 <19002046@alumnos.utleon.edu.mx>
2024-02-15 01:25:29 -0600

    "CMRT 14/02/24"

--- forms.py
+++ forms.py
@@ -1,27 +1,23 @@
-from wtforms import Form
-from wtforms import StringField, TelField, IntegerField
-
-from wtforms import EmailField
-from wtforms.validators import DataRequired, Email
+from wtforms import Form, StringField, IntegerField
+from wtforms.validators import DataRequired, Length, Email
 
 class UserForm(Form):
-    nombre = StringField('nombre', [
-        validators.DataRequired(message='El campo es obligatorio'),
-        validators.Length(min=4, max=10, message='Ingresa un nombre válido')
+    nombre = StringField('Nombre', validators=[
+        DataRequired(message="El nombre es requerido"),
+        Length(min=4, max=50, message="Ingresa un nombre válido (entre 4 y 50 caracteres)")
     ])
-    email = StringField('email', [
-        validators.DataRequired(message='El campo es obligatorio'),
-        validators.Email(message='Ingresa un email válido')
+    email = StringField("Email", validators=[
+        DataRequired(message="El email es requerido"),
+        Email(message="Ingresa un email válido")
     ])
-    apaterno = StringField('apaterno', [
-        validators.DataRequired(message='El campo es requerido'),
-        validators.Length(min=4, max=30, message='Ingresa un apellido válido')
+    apaterno = StringField("Apellido Paterno", validators=[
+        DataRequired(message="El apellido paterno es requerido"),
+        Length(min=4, max=50, message="Ingresa un apellido paterno válido (entre 4 y 50 caracteres)")
     ])
-    amaterno = StringField('amaterno', [
-        validators.DataRequired(message='El campo es requerido'),
-        validators.Length(min=4, max=10, message='Ingresa un apellido válido')
+    amaterno = StringField('Apellido Materno', validators=[
+        DataRequired(message="El apellido materno es requerido"),
+        Length(min=4, max=50, message="Ingresa un apellido materno válido (entre 4 y 50 caracteres)")
     ])
-    edad = IntegerField('edad', [
-        validators.DataRequired(message='El campo es requerido'),
-        validators.NumberRange(min=1, max=5, message='Ingresa una edad válida')
+    edad = IntegerField('Edad', validators=[
+        DataRequired(message="La edad es requerida")
     ])
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask,render_template,request
 import forms
-
 from wtforms import validators
-
 
 app=Flask(__name__)
 
